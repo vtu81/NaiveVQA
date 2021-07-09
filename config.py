c6ad6cd7f24123b3f327ebb32b10f5b5fb79a5bc
vtu81 <48283858+vtu81@users.noreply.github.com>
2021-07-09 14:15:32 +0800

    added data processing codes

---
+++ config.py
@@ -0,0 +1,26 @@
+# paths
+q_path = 'data/questions' # directory of questions
+a_path = 'data/annotations' # directory of annotations
+train_path = 'data/images/train'  # directory of training images
+val_path = 'data/images/val'  # directory of validation images
+test_path = 'data/images/test'  # directory of test images
+preprocessed_path = './resnet-14x14.h5'  # path where preprocessed features are saved to and loaded from
+vocabulary_path = 'vocab.json'  # path where the used vocabularies for question and answers are saved to
+
+task = 'OpenEnded'
+dataset = 'mscoco'
+
+# preprocess config
+preprocess_batch_size = 4
+image_size = 448  # scale shorter end of image to this size and centre crop
+output_size = image_size // 32  # size of the feature maps after processing through a network
+output_features = 2048  # number of feature maps thereof
+central_fraction = 0.875  # only take this much of the centre when scaling and centre cropping
+
+# training config
+epochs = 50
+batch_size = 128
+initial_lr = 1e-3  # default Adam lr
+lr_halflife = 50000  # in iterations
+data_workers = 8
+max_answers = 3000
---
+++ preprocess-vocab.py
@@ -0,0 +1,65 @@
+import enum
+import json
+from collections import Counter
+import itertools
+
+import config
+import data
+import utils
+
+
+def extract_vocab(iterable, top_k=None, start=0):
+    """ Turns an iterable of list of tokens into a vocabulary.
+        These tokens could be single answers or word tokens in questions.
+    """
+    all_tokens = itertools.chain.from_iterable(iterable)
+    counter = Counter(all_tokens)
+    if top_k:
+        most_common = counter.most_common(top_k)
+        most_common = (t for t, c in most_common)
+    else:
+        most_common = counter.keys()
+    # descending in count, then lexicographical order
+    tokens = sorted(most_common, key=lambda x: (counter[x], x), reverse=True)
+    vocab = {t: i for i, t in enumerate(tokens, start=start)}
+    return vocab
+
+
+def main():
+    questions = utils.path_for(train=True, question=True)
+    answers = utils.path_for(train=True, answer=True)
+
+    with open(questions, 'r') as fd:
+        questions = json.load(fd)
+    with open(answers, 'r') as fd:
+        answers = json.load(fd)
+
+    questions = data.prepare_questions(questions)
+    answers = data.prepare_answers(answers)
+
+    question_vocab = extract_vocab(questions, start=1)
+    answer_vocab = extract_vocab(answers, top_k=config.max_answers)
+
+    print("Questions' vocabulary size: ", len(question_vocab))
+    print("Top 5 words in questions:")
+    for index, word in enumerate(question_vocab):
+        print("\'{}\'\tid: {}".format(word, index))
+        if index > 4: break; 
+
+    print("Answers' vocabulary size: ", len(answer_vocab))
+
+    print("Top 5 answers:")
+    for index, word in enumerate(answer_vocab):
+        print("\'{}\'\tid: {}".format(word, index))
+        if index > 4: break; 
+
+    vocabs = {
+        'question': question_vocab,
+        'answer': answer_vocab,
+    }
+    with open(config.vocabulary_path, 'w') as fd:
+        json.dump(vocabs, fd)
+
+
+if __name__ == '__main__':
+    main()
